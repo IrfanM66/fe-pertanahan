a058863a438811e2d745b1be08ae2f843a62e3bc
Hermawan Sutanto <114162839+HermawanSutanto@users.noreply.github.com>
2024-06-21 00:10:39 +0700

    Update FetchSuratMasuk.js

--- src/utils/FetchSuratMasuk.js
+++ src/utils/FetchSuratMasuk.js
@@ -1,4 +1,4 @@
-const url = "https://api.persuratankantah.xyz/api";
+const url = "https://simdiskantahjember.com/api";
 
 const GetSuratMasuk = async (page) => {
   const response = await fetch(url + "/show-letters?page=" + page, {
