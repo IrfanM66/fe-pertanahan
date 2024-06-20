280607ef9a40f1cee4ae4f84a6df68e23c6ffc16
Hermawan Sutanto <114162839+HermawanSutanto@users.noreply.github.com>
2024-06-21 00:10:27 +0700

    Update FetchRekapSurat.js

--- src/utils/FetchRekapSurat.js
+++ src/utils/FetchRekapSurat.js
@@ -1,4 +1,4 @@
-const url = "https://api.persuratankantah.xyz/api";
+const url = "https://simdiskantahjember.com/api";
 
 const GetRekapSurat = async (page) => {
   const response = await fetch(url + "/show-rekap?page=" + page, {
